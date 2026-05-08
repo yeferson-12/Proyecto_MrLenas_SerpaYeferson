c7ac8be7cd95310e27b75b7f13c453911b9a8e78
Yefferson-12 <71551951@continental.edu.pe>
2026-05-08 00:06:05 -0500

    fix: use VITE_API_URL env variable for production

--- mr-lenas-frontend/src/services/api.js
+++ mr-lenas-frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: import.meta.env.VITE_API_URL 
+    ? `${import.meta.env.VITE_API_URL}/api` 
+    : 'http://127.0.0.1:8000/api',
   headers: { 
     'Content-Type': 'application/json',
     'Accept': 'application/json',
